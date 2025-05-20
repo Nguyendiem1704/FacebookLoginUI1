26b0975e8dbc3d64be940d53458bf7ceefa22872
LENOVO <nguyendiem17042003@gmail.com>
2025-05-20 16:36:40 +0700

    finish

--- app/src/main/java/com/example/facebookloginui/MainActivity.kt
+++ app/src/main/java/com/example/facebookloginui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.facebookloginui
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +10,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username == "admin" && password == "123456") {
+                Toast.makeText(this, "Login successfully", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
